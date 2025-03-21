8f03ebea039dc3e232d48fb2f22d2693a1e98d02
balamurugan9655 <shanmugambala80@gmail.com>
2025-03-21 11:23:21 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Container } from "react-bootstrap";
 
 const App = () => {
     return(
-        <Router>
+        <Router basename="React-MySQL-CURD-Client">
             <Container fluid>
                 <NavigationBar/>
                 <Container>
