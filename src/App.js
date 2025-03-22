2182f4139f3da720c768f293f0471bd4bc22bb4d
balamurugan9655 <shanmugambala80@gmail.com>
2025-03-22 18:33:04 +0530

    Updated remote repo name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Container } from "react-bootstrap";
 
 const App = () => {
     return(
-        <Router basename="React-MySQL-CURD-Client">
+        <Router basename="React-MySQL-CRUD-Client">
             <Container fluid>
                 <NavigationBar/>
                 <Container>
